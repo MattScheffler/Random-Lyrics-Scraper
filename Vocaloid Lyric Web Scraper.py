c685f0df74f3042fddf491426deef22e2cb2169b
MattScheffler <mattscheffler4@gmail.com>
2019-08-25 10:34:43 -0500

    Add files via upload

--- Vocaloid Lyric Web Scraper.py
+++ Vocaloid Lyric Web Scraper.py
@@ -30,44 +30,57 @@ web_soup = BeautifulSoup(web_data, "lxml")
 
 print(web_soup.prettify())
 '''
-### vlw_ will be for "Vocaloid Lyrics Wiki" ###
-vlw_random_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/ゴキブリの味_(Gokiburi_no_Aji)").text
-#vlw_random_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/Special:Random").text
-vlw_soup = BeautifulSoup(vlw_random_song, "lxml")
 
-if ("(disambiguation)" in vlw_soup.title.text):
-    print("Disambiguation page found")
-    print(vlw_soup.title.text)
-    print("Link:",vlw_soup.find("link", rel="canonical").get("href"))
-else:
-    try:
-        title_format = vlw_soup.title.text.find("|")
-        singers = []
-        for singer in vlw_soup.find_all("a", class_ = "mw-redirect"):
-            singers.append(singer.text)
-        
-        print("Title:", vlw_soup.title.text[:title_format].strip())
-        print("Singer(s): ", end = "")
-        for singer in range(0,len(singers)-1):
-            if (singer == len(singers)-2):
-                print(singers[singer])
-            else:
-                print(singers[singer], end = ", ")
-        print(f"Producer: {singers[len(singers)-1]}")
+def vlw_song_get():
+    ### vlw_ will be for "Vocaloid Lyrics Wiki" ###
+    '''Song with Japanese, Romaji, and English'''
+    #vlw_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/ゴキブリの味_(Gokiburi_no_Aji)").text
+    '''Song with Japanese and Romaji only'''
+    #vlw_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/闇屋の娘は眼で殺す_(Yamiya_no_Musume_wa_Me_de_Korosu)").text
+    '''Song with only one language'''
+    #vlw_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/I%27m_Breathless").text
+    '''Random song, will be the final choice used when finished'''
+    vlw_song = requests.get("https://vocaloidlyrics.fandom.com/wiki/Special:Random").text
+    
+    vlw_soup = BeautifulSoup(vlw_song, "lxml")
+    # First handle if an individual song's page isn't selected
+    if ("(disambiguation)" in vlw_soup.title.text):
+        print("Disambiguation page found")
+        print(vlw_soup.title.text)
         print("Link:",vlw_soup.find("link", rel="canonical").get("href"))
+    else:
         try:
-            #print(vlw_soup.find("table", style = "width:100%").text)
-            skip_line = ["Japanese", "Romaji", "English"]
-            for lyric in vlw_soup.find("table", style = "width:100%").stripped_strings:
-                if (lyric in skip_line):
-                    pass
-                else:
-                    print(lyric)
-        except AttributeError:
-            print(vlw_soup.find("div", class_ = "poem").text)
-    except Exception as e:
-        print(e)
+            title_format = vlw_soup.title.text.find("|")
+ 
+            print("Title:", vlw_soup.title.text[:title_format].strip())
+            
+            print(vlw_soup.find("b", text = "Producer(s)").text + ":")
+            print(vlw_soup.find("b", text = "Producer(s)")
+                  .next_element.next_element.next_element.next_element.text.strip())
 
-##with codecs.open("Lyrics_Test_File_Stripped.txt","w", encoding = "utf-8") as f:
-##    for lyric in vlw_soup.find("table", style = "width:100%").stripped_strings:
-##        f.write(lyric)
+            print(vlw_soup.find("b", text = "Singer").text + "(s):")
+            print(vlw_soup.find("b", text = "Singer")
+                  .next_element.next_element.next_element.next_element.text.strip())
+
+            print("Link:",vlw_soup.find("link", rel="canonical").get("href"))
+            print()
+            try:
+                #print(vlw_soup.find("table", style = "width:100%").text)
+                skip_line = ["Japanese", "Romaji", "English"]
+                for lyric in vlw_soup.find("table", style = "width:100%").stripped_strings:
+                    if (lyric in skip_line):
+                        pass
+                    else:
+                        print(lyric)
+            except AttributeError:
+                '''For when there is only one language. No further formatting needed.'''
+                print(vlw_soup.find("div", class_ = "poem").text)
+        except Exception as e:
+            print(e)
+
+    '''Save lyrics to file for testing, may leave the option in the finished program.'''
+    ##with codecs.open("Lyrics_Test_File_Stripped.txt","w", encoding = "utf-8") as f:
+    ##    for lyric in vlw_soup.find("table", style = "width:100%").stripped_strings:
+    ##        f.write(lyric)
+
+vlw_song_get()
